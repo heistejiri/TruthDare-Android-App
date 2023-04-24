8cd7cf4c6840097a70a3452c260f0a25d14d0e89
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-04-24 21:11:33 +0100

    Create TruthActivity.java

---
+++ app/src/main/java/com/heistejiri/truthdareapp/TruthActivity.java
@@ -0,0 +1,14 @@
+package com.heistejiri.truthdareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TruthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truth);
+    }
+}
