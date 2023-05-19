6ed1603f199873d324c2adfa86c55a6fcf09101d
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-05-19 18:22:04 +0100

    Create TruthItem.java

---
+++ app/src/main/java/com/heistejiri/truthdareapp/TruthItem.java
@@ -0,0 +1,15 @@
+package com.heistejiri.truthdareapp;
+
+public class TruthItem{
+
+    private String mText;
+
+    public TruthItem(String text) {
+        mText = text;
+    }
+
+    public String getmText() {
+        return mText;
+    }
+
+}
